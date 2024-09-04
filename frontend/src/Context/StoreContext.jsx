915daaac8c1395f7fa2af94fb52e605786d524aa
Haripriyaa <mailtohari1224@gmail.com>
2024-09-04 23:17:50 +0530

    Connection DB with UI for cart management

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -16,19 +16,25 @@ const StoreContextProvider = (props)=>{
     const url = "http://localhost:10000";
 
     //addtocart functionality
-    const addToCart =(itemId)=>{
+    const addToCart = async (itemId)=>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev, [itemId]: 1}))
         }
         else{
             setCartItems((prev)=>({...prev, [itemId]:prev[itemId]+1}))
         }
+        if(token){
+            await axios.post(url+'/api/cart/add',{itemId}, {headers:{token}})
+        }
     }
 
     //remove cart
 
-    const removeFromCart =(itemId)=>{
+    const removeFromCart = async (itemId)=>{
         setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
+        if(token){
+            await axios.post(url+'/api/cart/remove',{itemId}, {headers:{token}})
+        }
     }
 
     // useEffect(()=>{
